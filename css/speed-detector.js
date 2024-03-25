6a29e40a8b7eae15e6916d430c5f3541f5855565
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-25 22:31:58 +0300

    adds to speed-detector.js file and comments

--- css/speed-detector.js
+++ css/speed-detector.js
@@ -1,12 +1,20 @@
-//function declaration for print grades function
-function calculateDemerit(speed) {
-//declare and initialize speed
-    const speed = 70;
-//introduce conditional statement to check for speed more than 70km/hr
-    if (speed <=70) {
-        console.log("OK")
-      }
-      
-      }
-// function call with speed argument
-calculateDemerit(5)
+//function declaration for points deduction function
+function calculateDemerit(speed){
+//declare and initialize speed limit
+    const limit = 70
+//introduce conditional statement to check for speed less or equal to 70km/hr
+    if(speed <=limit) {
+        return `Ok`
+     }
+//alternate statement to calculate deduction points for speed more than speed limit and points more than 12
+//five km/hr equal 1 point
+    else if(((speed-limit)/5)>12){
+        return `License suspended`
+     }
+//last statement to catch speeds more than the limit but calculated points less than 12   
+    else {
+        return `Points:`+ (speed-limit)/5
+     }
+ }
+ //function call
+ calculateDemerit(80)
