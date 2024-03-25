ab74b48013655fb549cfc9e1e4e1dfc14b9b6a94
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-25 22:44:43 +0300

    adds commentary for grade-generator.js file

--- css/grade-generator.js
+++ css/grade-generator.js
@@ -1,25 +1,31 @@
 //function declaration for print grades function
 function printGrades(marks) {
 //introduce conditional statement to check for marks between 0 and 100
-if (marks >=0 && marks < 40) {
-    console.log("E")
-  }
-  if (marks >=40 && marks <49) {
-        console.log("D")
-  }
-  if (marks >=50 && marks <=59) {
-        console.log("C")
-  }
-  if (marks >=60 && marks <=79) {
-        console.log("B")
-  }
-  if (marks >79 && marks <=100) {
-            console.log("A")
-  }
-  //Last statement to catch anything outside of 0 - 100 marks
-  if (marks <0 || marks > 100) {
-  console.log("Number out of range!!")
-  }
-  }
- // function call with marks argument
-  printgrades(-1)
+    if (marks >=0 && marks < 40)  {
+        console.log ("E")
+        return "E"
+        }
+    if (marks >=40 && marks <49)  {
+        console.log ("D")
+        return "D"
+        }
+    if (marks >=50 && marks <=59) {
+        console.log ("C")
+        return "C"
+        }
+    if (marks >=60 && marks <=79) {
+        console.log ("B")
+        return "B"
+        }
+    if (marks >79 && marks <=100) {
+        console.log ("A")
+        return "A"
+        }
+//Last statement to catch anything outside of 0 - 100 marks
+    if (marks <0 || marks > 100)  {
+        console.log ("Number out of range!!")
+        return "Number out of range!!"
+        }
+}
+// function call with marks argument
+printGrades(-1)
